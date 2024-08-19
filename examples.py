7bfb8b0ecb73c6850b89be8097a77ede0c55c412
Becse508 <kisbecse@gmail.com>
2024-08-19 22:47:09 +0200

    fixed some renaming issues

--- examples.py
+++ examples.py
@@ -4,26 +4,26 @@ from random import randint
 
 # EVENTS
 
-myasset = assets.Sprite(rect = '')
+mysprite = assets.Sprite(rect = '')
 
 
 # Default Event
-@myasset.event()
-def onhover(asset: assets.Sprite, isHovered: bool): # the parameters are always these
+@mysprite.event()
+def onhover(sprite: assets.Sprite, isHovered: bool): # the parameters are always these
     if isHovered:
-        asset.image.fill((255,0,0))
+        sprite.image.fill((255,0,0))
     else:
-        asset.image.fill((255,255,255))
+        sprite.image.fill((255,255,255))
 
 
 
 # Custom Event
 
-# checks if the mouse is sitting on top of the asset and SPACE is pressed (runs once per press).
-mycheck = lambda asset,event: asset.rect.colliderect(pygame.mouse.get_pos()) and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE
+# checks if the mouse is sitting on top of the sprite and SPACE is pressed (runs once per press).
+mycheck = lambda sprite,event: sprite.rect.colliderect(pygame.mouse.get_pos()) and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE
 
-@myasset.event(check = mycheck, name = 'space_pressed') # name is optional
-def pressing_space(asset: assets.Sprite, pressing_it: bool): # the parameters are always these
+@mysprite.event(check = mycheck, name = 'space_pressed') # name is optional
+def pressing_space(sprite: assets.Sprite, pressing_it: bool): # the parameters are always these
     if pressing_it:
         # do some stuff
-        myasset.image.fill((randint(0,255),randint(0,255),randint(0,255)))
+        mysprite.image.fill((randint(0,255),randint(0,255),randint(0,255)))
